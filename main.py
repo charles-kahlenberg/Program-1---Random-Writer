dc1e6d1c6651285d166858b36c851939d09a4741
charles kahlenberg <charliekahlenberg@gmail.com>
2026-09-01 12:45:44 -0400

    adding texts and header to prog

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Charles Kahlenberg 
+Date Created: 9/1/2026
+Last edit: 9/1/2026
+
+Random Writer: Collects information from a provided text and creates a predictive model based on it to create an output.
+"""
+
+# INPUTS
+import random
+
+# SUBROUTINES
+
+# VARIABLES
+
+###### MAIN ######
+
+# OUTPUT
